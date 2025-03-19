5b50447f460c4260bd3481af48ab398d008a2491
te6lim <teslimakande@gmail.com>
2025-03-20 00:35:28 +0100

    store environment variables

--- database/database.go
+++ database/database.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type chatDB struct {
@@ -15,10 +18,18 @@ var mockdb *chatDB
 
 var Instance *sql.DB
 
-var ConnURL = "postgres://postgres:plantainchips@localhost:5432/go-chat"
-
 func ConnectToDB() {
 	if Instance == nil {
+		envErr := godotenv.Load()
+		if envErr != nil {
+			log.Fatal("Error loading .env file")
+		}
+		var dbUser = os.Getenv("DB_USER")
+		var dbPassword = os.Getenv("DB_PASSWORD")
+		var dbHost = os.Getenv("DB_HOST")
+		var dbPort = os.Getenv("DB_PORT")
+		var dbName = os.Getenv("DB_NAME")
+		var ConnURL = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 		newdb, err := sql.Open("pgx", ConnURL)
 		if err != nil {
 			log.Fatal()
